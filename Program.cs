20b41aa8a95fc364f1edcd148b6a6c2e5812126d
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2023-07-11 15:04:51 -0600

    use https redirect

--- Program.cs
+++ Program.cs
@@ -6,13 +6,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-    app.UseHttpsRedirection();
-}
+
+app.UseHsts();
+app.UseHttpsRedirection();
+
 
 app.UseStaticFiles();
 app.UseRouting();
@@ -22,6 +19,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
 
-app.MapFallbackToFile("index.html");;
+app.MapFallbackToFile("index.html");
 
 app.Run();
